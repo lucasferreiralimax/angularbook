94fc90766e77a394b60eb88f47788b1eb510411a
Lucas Lima <lucaslimay@gmail.com>
2018-09-14 22:50:27 -0300

    ajuste nos estilos

--- src/app/view/amigos/amigos.component.ts
+++ src/app/view/amigos/amigos.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-amigos',
   templateUrl: './amigos.component.html',
-  styleUrls: ['./amigos.component.sass']
+  styleUrls: ['./amigos.component.sass'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AmigosComponent implements OnInit {
 
