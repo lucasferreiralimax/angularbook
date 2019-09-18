231ce679cf605fbda2f6060b838c9de35b135f5f
Lucas Ferreira Lima <lucasferreiralimax@gmail.com>
2019-09-17 21:16:34 -0300

    movendo nav para dentro do header

---
+++ src/app/components/header/nav/nav.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+import { UsuarioService } from '../../../services/usuario.service'
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.sass']
+})
+export class NavComponent implements OnInit {
+
+  constructor(private usuarioService:UsuarioService) { }
+
+  user = {}
+
+  ngOnInit() {
+    this.user = this.usuarioService.getUser()
+  }
+
+}
