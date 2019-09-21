df03b7bf443023a57c78e11c42db3b28f034f6e9
Lucas Ferreira Lima <lucasferreiralimax@gmail.com>
2019-09-20 21:51:03 -0300

    importando HTTPModule :+1:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './modules/core/core.module';
 import { AppComponent } from './app.component';
 
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     CoreModule
   ],
   providers: [],
