6324452b1f94ef92081f81d46ce7ae3cc8b1379e
lucasferreiralimax <lucasferreiralimax@gmail.com>
2019-10-05 23:29:05 -0300

    add component reacts

---
+++ src/app/components/feed/reacts/reacts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reacts',
+  templateUrl: './reacts.component.html',
+  styleUrls: ['./reacts.component.sass']
+})
+export class ReactsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -3,15 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { FooterComponent } from '@components/footer/footer.component';
 import { LoadingComponent } from '@components/loading/loading.component';
+import { ReactsComponent } from '@components/feed/reacts/reacts.component';
 
 @NgModule({
   declarations: [
     FooterComponent,
-    LoadingComponent
+    LoadingComponent,
+    ReactsComponent
   ],
   exports: [
     FooterComponent,
-    LoadingComponent
+    LoadingComponent,
+    ReactsComponent
   ],
   imports: [
     CommonModule
