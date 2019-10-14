8b8ebe844f5837356bafd36ee98ddea411b1dbc7
lucasferreiralimax <lucasferreiralimax@gmail.com>
2019-10-14 00:35:47 -0300

    import components in sharedmodule

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,23 +1,40 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router'
 
 import { FooterComponent } from '@components/footer/footer.component';
 import { LoadingComponent } from '@components/loading/loading.component';
+import { FeedComponent } from '@components/feed/feed.component';
+import { PostComponent } from '@components/feed/post/post.component';
+import { CommentComponent } from '@components/feed/comment/comment.component';
 import { ReactsComponent } from '@components/feed/reacts/reacts.component';
+import { AboutComponent } from '@components/about/about.component';
+import { PostCommentDirective } from '@directives/postcomment.directive';
 
 @NgModule({
   declarations: [
     FooterComponent,
     LoadingComponent,
-    ReactsComponent
+    FeedComponent,
+    PostComponent,
+    CommentComponent,
+    ReactsComponent,
+    AboutComponent,
+    PostCommentDirective
   ],
   exports: [
     FooterComponent,
     LoadingComponent,
-    ReactsComponent
+    FeedComponent,
+    PostComponent,
+    CommentComponent,
+    ReactsComponent,
+    AboutComponent,
+    PostCommentDirective
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
 export class SharedModule { }
