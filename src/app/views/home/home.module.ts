9e5001eb96ed1407118efc1074852bc51ed30a90
lucasferreiralimax <lucasferreiralimax@gmail.com>
2019-10-14 00:39:36 -0300

    remove modules in homemodule

--- src/app/views/home/home.module.ts
+++ src/app/views/home/home.module.ts
@@ -4,11 +4,6 @@ import { CommonModule } from '@angular/common'
 import { HomeComponent } from './home.component'
 import { NavleftComponent } from '@components/navleft/navleft.component';
 import { AsideComponent } from '@components/aside/aside.component';
-import { FeedComponent } from '@components/feed/feed.component';
-import { PostComponent } from '@components/feed/post/post.component';
-import { CommentComponent } from '@components/feed/comment/comment.component';
-import { PostCommentDirective } from '@directives/postcomment.directive';
-
 import { HomeRoutingModule } from './home-routing.module'
 import { SharedModule } from '@modules/shared/shared.module';
 
@@ -21,11 +16,7 @@ import { SharedModule } from '@modules/shared/shared.module';
   declarations: [
     HomeComponent,
     NavleftComponent,
-    AsideComponent,
-    FeedComponent,
-    PostComponent,
-    CommentComponent,
-    PostCommentDirective
+    AsideComponent
   ]
 })
 export class HomeModule { }
